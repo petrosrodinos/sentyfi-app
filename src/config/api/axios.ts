b46225554dc77514920ea86ffdff27fe37215e57
Petros <petros.rodinos@yahoo.com>
2025-07-14 20:36:52 +0300

    token expiratin and logout

--- src/config/api/axios.ts
+++ src/config/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { API_URL } from '@/config/api/routes'
 import { getAuthStoreState } from '@/stores/auth'
+import { isTokenExpired } from '@/lib/token'
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
@@ -12,9 +13,15 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use((config) => {
     const authState = getAuthStoreState();
+
+    if (authState?.expires_in && isTokenExpired(authState.expires_in)) {
+        authState.logout();
+    }
+
     if (authState.access_token) {
         config.headers.Authorization = `Bearer ${authState.access_token}`;
     }
+
     return config;
 });
 
